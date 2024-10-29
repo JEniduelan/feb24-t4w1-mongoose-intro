24e73bbd45ce7a722f192c2ae80885ff8d0ffadb
John Niduelan <jeniduelan@gmail.com>
2024-10-29 21:02:10 +1100

    feat: connect to mongodb

--- src/index.js
+++ src/index.js
@@ -1,17 +1,25 @@
-// Purpose: index.js
-// First point of entry
-// Initiate the server
+// Purpose: 
+// First point of entry 
+// Initiate the server 
 // Get the port
-// Tell the server to listen to web traffic
+// Tell the server to listen to web traffic 
 
 // Server is configured in this file, not in index.js: 
 const {app} = require("./server.js");
+const { dbConnect } = require("./utils/database.js");
 
-//  get the PORT value from environment variables
+// Get the PORT value from environment variables
 const PORT = process.env.PORT || 8080;
 
+// dbConnect().then(() => {
+// 	app.listen();
+// })
+
 // app.listen(port, callback)
-app.listen(PORT, () => {
+app.listen(PORT, async () => {
+
+	await dbConnect();
+
 	// Server is running if we reach this point in the code!
 	console.log("Server is running on port " + PORT);
 });
